cb06f1160546be09a7e9526379e921ecfdcb7012
maaldanac@gmail.com <maaldanac@gmail.com>
2026-03-01 23:13:46 +0000

    applied ruff format

--- src/openfema/utils/ids.py
+++ src/openfema/utils/ids.py
@@ -1,12 +1,14 @@
 import uuid
 from datetime import datetime, timezone
 
+
 def new_load_id() -> str:
     return str(uuid.uuid4())
+
 
 def utc_ingest_date() -> str:
     return datetime.now(timezone.utc).date().isoformat()
 
+
 def utc_now_ts() -> str:
     return datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
-    
